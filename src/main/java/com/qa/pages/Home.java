b74a101191de7e042da23b8c1c9a9df003440a39
sandhiya palanisamy <sandhiya.palanisamy@gmail.com>
2019-02-01 23:52:52 -0600

    first git commit for my selenium code

---
+++ src/main/java/com/qa/pages/Home.java
@@ -0,0 +1,7 @@
+package com.qa.pages;
+
+public class Home {
+public void homepage() {
+	System.out.println("homepage method");
+}
+}
---
+++ src/test/java/com/qa/testpacakge/loginTest.java
@@ -0,0 +1,5 @@
+package com.qa.testpacakge;
+
+public class loginTest {
+
+}
